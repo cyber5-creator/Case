bf7f4870890f88a36ef9a2c9216eef60c658f427
cyber5-creator <nyork2820@gmail.com>
2024-02-22 05:57:01 +0700

    first commit

---
+++ settings.js
@@ -0,0 +1,84 @@
+const version = require("@whiskeysockets/baileys/package.json").version
+
+global.baileys = require('@whiskeysockets/baileys') 
+global.language = "id"
+global.nomerOwner = ["6282163617462"]
+global.ownerjid = "6282163617462@s.whatsapp.net"
+global.sig = "https://instagram.com/thaipuri"
+global.email = "nyork2820@gmail.com"
+global.runWith = "Panel"
+global.botName = "ⓐⓝⓘⓝⓓⓨⓐ" 
+global.ownerName = "ⓣⓗⓐⓘⓕⓤⓡⓘ" 
+global.sessionName ="session"
+global.setmenu ="location" 
+global.docType = "docx"
+global.prefa = "!"
+global.fake = botName
+global.copyright = `Bot WhatsApp`
+global.baileysVersion = `Baileys ${version}`
+global.On = "Off"
+global.Off ="Off"
+global.fake1 ="ᴀɴɪɴᴅʏᴀ°᭄ᴮᵒᵗ`"
+global.packName = "ᴏɴғɪᴄᴇ"
+global.authorName = "Created By ᴀɴɪɴᴅʏᴀ"
+global.replyType = "mess"
+global.setwelcome = "type1"
+global.Dana = '6282163617462'
+global.gamewaktu = 60
+global.limitCount = 30
+global.Intervalmsg = 1000 //detik
+global.sgc = "https://chat.whatsapp.com/FEIUU6sLelZBVawsoFR8RP"
+global.syt = 'https://whatsapp.com/channel/0029VaKRNVkIHphFdldqZS25"
+global.nobg = ['jVW2tQEAj2Qo7RD2mjcv99QU','Z9XYXF8LgFeszUF3a6u5PKbJ', 'yFPL4CQFi38fAWVhVQczyrBg']
+global.token = "13cc2eee-5315-4d84-b3c8-d5b05ce297c3"
+global.fileStackApi = "AVKHbeyXsT0G9IKI01qenz"  
+global.usePairingCode = true 
+global.pairingNumber = "6282163617462"  
+global.public = true
+global.baileysMd = true
+global.antiSpam = true
+global.multi = true
+global.autoblockcmd = false
+global.autoReport = true
+global.autoLevel = true
+global.autoSticker = false 
+global.Console = false
+global.autorespon = false
+global.autoblockcmd = false
+//Apikey kalo error daftar aje
+global.api = {
+alice: 'always ehz', //skizo.tech
+angel: 'zenzkey_af003aedbf', // Apikey Zahwazein
+Lol: 'GataDios',
+Botcahx: 'Admin',
+Apiflash: '9b9e84dfc18746d4a19d3afe109e9ea4',
+}
+global.gcounti = {
+'prem' : 1000,
+'user' : 20
+} 
+global.fotoRandom = [
+    "https://telegra.ph/file/0ada53a88a1617b24e1c2.jpg",
+    "https://telegra.ph/file/d0c414f12d006486256e8.jpg",
+    "https://telegra.ph/file/8dd923a69f562d5264b3b.jpg",
+    "https://telegra.ph/file/f6840e7b9c789bd6f7b75.jpg",
+    "https://telegra.ph/file/a2b84d6ab76d01cefa658.jpg",
+   
+    ]
+
+const fs = require("fs");
+const { color } = require("./lib/color");
+const chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
+
+
+
+
+
+
